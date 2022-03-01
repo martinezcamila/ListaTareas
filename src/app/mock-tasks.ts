3ded486565e49d28f92339274c4ff737fb669c3d
Camila <camila.martinez.sist@gmail.com>
2022-03-01 18:51:45 -0300

    header

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,31 @@
+import { Task } from "./Task"
+export var TASKS:Task[] = [
+    {
+        id:1,
+        text:"Desayunar",
+        day: "01/03/2022",
+        time: "09:00 AM",
+        reminder: true
+    },
+    {
+        id:2,
+        text:"Almorzar",
+        day: "01/03/2022",
+        time: "13:00 PM",
+        reminder: true
+    },
+    {
+        id:3,
+        text:"Estudiar",
+        day: "01/03/2022",
+        time: "14:00 PM",
+        reminder: true
+    },
+    {
+        id:4,
+        text:"Merendar",
+        day: "01/03/2022",
+        time: "17:00 PM",
+        reminder: true
+    }
+]
